7062abbd8e73a8b7981c86105931bb22e392e4b5
HugoHtvl <hug.oo@hotmail.fr>
2023-05-31 11:10:33 +0200

    ajout de commentaires

--- src/components/Pokemoncard.jsx
+++ src/components/Pokemoncard.jsx
@@ -1,26 +1,28 @@
 const pokemonList = [
   {
     name: "bulbasaur",
-
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
-
   {
     name: "mew",
   },
 ];
 
 function Pokemoncard() {
+  // Sélectionne le premier élément du tableau pokemonList
   const pokemon = pokemonList[0];
 
   return (
     <figure>
       {pokemon.imgSrc != undefined ? (
+        // Si l'image du Pokémon est définie, affiche une balise img avec la source et l'attribut alt correspondants
         <img src={pokemon.imgSrc} alt={pokemon.name} />
       ) : (
+        // Sinon, affiche un paragraphe avec le texte "???"
         <p>???</p>
       )}
+      {/* Affiche le nom du Pokémon */}
       <figcaption>{pokemon.name}</figcaption>
     </figure>
   );

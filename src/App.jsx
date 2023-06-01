d4738fb204c5224067b47160dbb2219aa5c11228
HugoHtvl <hug.oo@hotmail.fr>
2023-06-01 18:58:53 +0200

    création des boutons interactifs - quete 8

--- src/App.jsx
+++ src/App.jsx
@@ -8,55 +8,49 @@ import PokemonCard from "./components/PokemonCard";
 import NavBar from "./components/NavBar";
 import "./App.css";
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-
-  {
-    name: "charmander",
-
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-  },
-
-  {
-    name: "squirtle",
-
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-  },
-
-  {
-    name: "pikachu",
-
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-  },
-
-  {
-    name: "mew",
-  },
-];
-
 function App() {
   useEffect(() => {
     alert("Hello pokemon trainer 🙂");
   }, []);
 
+  const [pokemonList, setPokemonList] = useState([
+    {
+      name: "bulbasaur",
+
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+
+    {
+      name: "charmander",
+
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+
+    {
+      name: "squirtle",
+
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+
+    {
+      name: "pikachu",
+
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+
+    {
+      name: "mew",
+    },
+  ]);
+
   const [pokemonIndex, setPokemonIndex] = useState(0);
-  const handleClickPrevious = () => {
-    if (pokemonIndex > 0) {
-      setPokemonIndex(pokemonIndex - 1);
-    }
-  };
-  const handleClickNext = () => {
-    if (pokemonIndex < pokemonList.length - 1) {
-      setPokemonIndex(pokemonIndex + 1);
-    }
+
+  const handleClickPokemon = (index) => {
+    setPokemonIndex(index);
   };
 
   return (
@@ -64,10 +58,9 @@ function App() {
       <div>
         <MyTitle />
         <NavBar
-          pokemonIndex={pokemonIndex}
-          handleClickPrevious={handleClickPrevious}
-          handleClickNext={handleClickNext}
+          handleClickPokemon={handleClickPokemon}
           pokemonList={pokemonList}
+          pokemonIndex={pokemonIndex}
         />
         <PokemonCard pokemon={pokemonList[pokemonIndex]} />
       </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,19 @@
 // prettier-ignore
 
 const NavBar = (props) => {
-const { pokemonIndex, handleClickPrevious, handleClickNext, pokemonList } = props;
+const { handleClickPokemon, pokemonList, pokemonIndex } = props;
 
 setTimeout(() => {
   if (pokemonList[pokemonIndex].name === "pikachu") alert("Pika Pikachu !!!");
 }, "100")
 
   return (
-  <>
-    {pokemonIndex > 0 ? <button onClick={handleClickPrevious}>Previous</button> : <p></p>}
-    {pokemonIndex < pokemonList.length - 1 ? <button onClick={handleClickNext}>Next</button> : <p></p>}
+    <>
+    {pokemonList.map((pokemon, index) => (
+      <button key={index} onClick={() => handleClickPokemon(index)}>
+        {pokemon.name}
+      </button>
+    ))}
   </>
   )
 };

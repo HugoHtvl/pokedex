1f32778ab5fc1fe0855b1dc187d64256bfef1c78
HugoHtvl <hug.oo@hotmail.fr>
2023-06-01 16:04:42 +0200

    ajout des alertes - quete 7bis

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 // import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import MyTitle from "./components/MyTitle";
 import PokemonCard from "./components/PokemonCard";
 import NavBar from "./components/NavBar";
@@ -43,6 +43,10 @@ const pokemonList = [
 ];
 
 function App() {
+  useEffect(() => {
+    alert("Hello pokemon trainer 🙂");
+  }, []);
+
   const [pokemonIndex, setPokemonIndex] = useState(0);
   const handleClickPrevious = () => {
     if (pokemonIndex > 0) {
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,6 +3,10 @@
 const NavBar = (props) => {
 const { pokemonIndex, handleClickPrevious, handleClickNext, pokemonList } = props;
 
+setTimeout(() => {
+  if (pokemonList[pokemonIndex].name === "pikachu") alert("Pika Pikachu !!!");
+}, "100")
+
   return (
   <>
     {pokemonIndex > 0 ? <button onClick={handleClickPrevious}>Previous</button> : <p></p>}

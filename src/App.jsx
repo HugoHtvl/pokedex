dcbb97e16f56c14f6567448686eb98709c4c75c7
HugoHtvl <hug.oo@hotmail.fr>
2023-06-01 11:04:31 +0200

    ajout des différents Pokémon et des boutons - quete 6

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,66 @@
 // import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
+import { useState } from "react";
 import MyTitle from "./components/MyTitle";
 import Pokemoncard from "./components/Pokemoncard";
 import "./App.css";
 
 const pokemonList = [
   {
-    name: "bulbizarre",
+    name: "bulbasaur",
+
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
+
   {
-    name: "salamèche",
+    name: "charmander",
+
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
   },
+
+  {
+    name: "squirtle",
+
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+
+  {
+    name: "pikachu",
+
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+
   {
     name: "mew",
   },
 ];
 
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  const handleClickPrevious = () => {
+    if (pokemonIndex > 0) {
+      setPokemonIndex(pokemonIndex - 1);
+    }
+  };
+  const handleClickNext = () => {
+    if (pokemonIndex < pokemonList.length - 1) {
+      setPokemonIndex(pokemonIndex + 1);
+    }
+  };
+
+  // prettier-ignore
   return (
     <>
       <div>
         <MyTitle />
-        <Pokemoncard pokemon={pokemonList[1]} />
+        <Pokemoncard pokemon={pokemonList[pokemonIndex]} />
+        {pokemonIndex > 0 ? <button onClick={handleClickPrevious}>Previous</button> : <p></p>}
+        {pokemonIndex < pokemonList.length - 1 ? <button onClick={handleClickNext}>Next</button> : <p></p>}
       </div>
     </>
   );
